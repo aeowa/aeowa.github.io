c4487863f7e36a5a023a88f2a6952678ca3a3bf3
aeowa <aeowa@mail.ru>
2015-11-16 01:42:24 +0300

    html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
+// TABS
   $.ionTabs("#tabs_1", {type: "none"});
 
-
+// FORM AJAX
   $('#form-download').ajaxForm(function() { 
-                alert("Download link was sent to your e-mail");
-            });
+      alert("Download link was sent to your e-mail");
+      document.getElementById('form-download').reset();
+  });
 
+// POPUP
   $('#show').avgrund({
       height: 400,
       width: 800,
@@ -17,4 +20,26 @@ $(document).ready(function() {
       template: '<iframe width="600" height="350" src="https://www.youtube.com/embed/klModGpSKtc" frameborder="0" allowfullscreen></iframe>'
     });
 
+  $('#show2').avgrund({
+      height: 400,
+      width: 800,
+      holderClass: 'custom',
+      showClose: true,
+      showCloseText: 'close',
+      onBlurContainer: '.container',
+      template: '<iframe width="600" height="350" src="https://www.youtube.com/embed/klModGpSKtc" frameborder="0" allowfullscreen></iframe>'
+    });
+
 });
+
+// ADD SELECT 
+function addState(combo){
+
+   if(combo.value == 'USA'){
+     document.getElementById('state').style.display = 'inline-block';
+   }else{
+   document.getElementById('state').style.display = 'none';
+   }
+ }
+
+
